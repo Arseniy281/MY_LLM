0bb93264218b1caa110d1c69877bbc4b61a50c0b
Arseniy281 <araginskiy@bk.ru>
2026-08-13 01:25:05 +0300

    Phase 7: Positional Encoding + Universal Broadcast

--- Tensor/tensor_ops.cpp
+++ Tensor/tensor_ops.cpp
@@ -1,61 +1,106 @@
 #include "tensor.h"
+#include <vector>
+#include <cmath>
 
-bool IsBroadcastCompatible(const std::vector<size_t>& s1, const std::vector<size_t>& s2) {
-    auto to2D = [](const std::vector<size_t>& s) {
-        if (s.size() == 1) { return std::vector<size_t>{1, s[0]}; }
-        if (s.size() == 2) { return s; }
-        throw std::runtime_error("Broadcast only supports 1D and 2D tensors");
-    };
-    
-    std::vector<size_t> a = to2D(s1);
-    std::vector<size_t> b = to2D(s2);
-    
-    if (a[0] != b[0] && a[0] != 1 && b[0] != 1) { return false; }
-    if (a[1] != b[1] && a[1] != 1 && b[1] != 1) { return false; }
-    
-    return true;
+std::vector<size_t> AllignTensors(const std::vector<size_t>& small, const std::vector<size_t>& big) {
+    if (small.size() > big.size()) { return small; }
+    size_t final_size = big.size();
+    if (small.size() == big.size()) {
+        for (size_t i = 0; i < big.size(); i++) {
+            if (small[i] != big[i] && !(small[i] == 1 || big[i] == 1)) {
+                throw std::runtime_error("You cannot Broadcst this matrixes");
+            }
+        }
+        return small;
+    } else {
+        std::vector<size_t> new_small;
+        size_t i = 0;
+        size_t j = 0;
+        while (j < big.size()) {
+            if (small[i] == big[j] || small[i] == 1) {
+                new_small.push_back(small[i]);
+                i++;
+                j++;
+            } else {
+                new_small.push_back(1);
+                j++;
+            }
+        }
+        if (new_small.size() > final_size || j != big.size() || i != small.size()) {
+            throw std::runtime_error("You cannot Broadcst this matrixes");
+        }
+        return new_small;
+    }
 }
 
-Tensor Tensor::operator+(const Tensor& other) const { 
-    if (shape_.size() > 2 && other.shape_.size() > 2) {
-        if (shape_ != other.shape_) {
-            throw std::runtime_error("Shapes must match for >2D tensors");
+std::vector<size_t> GetFinalShape(const std::vector<size_t>& first, const std::vector<size_t>& second) {
+    if (first.size() != second.size()) {
+        throw std::runtime_error("You cannot Broadcst this matrixes");
+    }
+    std::vector<size_t> final_shape(first.size());
+    for (size_t i = 0; i < first.size(); i++) {
+        final_shape[i] = std::max(first[i], second[i]);
+    }
+    return final_shape;
+}
+
+size_t GetFinalSize(const std::vector<size_t>& final_shape) {
+    size_t final_size = 1;
+    for (const auto& dim : final_shape) {
+        final_size *= dim;
+    }
+    return final_size;
+}
+
+std::vector<size_t> IndexToCoord(size_t ind, const std::vector<size_t>& shape) {
+    std::vector<size_t> coords(shape.size());
+    for (int i = shape.size() - 1; i >= 0; i--) {
+        coords[i] = ind % shape[i];
+        ind /= shape[i];
+    }
+    return coords;
+}
+
+size_t CoordToIndex(const std::vector<size_t>& coord, const std::vector<size_t>& shape) {
+    size_t index = 0;
+    size_t stride = 1;
+    for (int i = shape.size() - 1; i >= 0; i--) {
+        index += coord[i] * stride;
+        stride *= shape[i];
+    }
+    return index;
+}
+
+size_t BroadcastIndex(const std::vector<size_t>& real_shape, const std::vector<size_t>& final_shape, size_t ind) {
+    std::vector<size_t> coords = IndexToCoord(ind, final_shape);
+    for (size_t i = 0; i < final_shape.size(); i++) {
+        if (real_shape[i] == 1) {
+            coords[i] = 0;
         }
-        Tensor result(shape_);
-        for (size_t i = 0; i < size_; i++) {
-            result.data_[i] = data_[i] + other.data_[i];
-        }
-        return result;
+    }
+    size_t real_index = CoordToIndex(coords, real_shape);
+    return real_index;
+}
+
+Tensor Tensor::operator+(const Tensor& other) const {
+    std::vector<size_t> s1 = shape_;
+    std::vector<size_t> s2 = other.GetShape();
+    s1 = AllignTensors(s1, s2);
+    s2 = (AllignTensors(s2, s1));
+
+    const float* data_1 = RawData();
+    const float* data_2 = other.RawData();
+
+    std::vector<size_t> final_shape = GetFinalShape(s1, s2);
+    size_t final_size = GetFinalSize(final_shape);
+
+    std::vector<float> final_data(final_size);
+    for (size_t i = 0; i < final_size; i++) {
+        final_data[i] = data_1[BroadcastIndex(s1, final_shape, i)] 
+                        + data_2[BroadcastIndex(s2, final_shape, i)];
     }
 
-    auto to2D = [](const Tensor& t) {
-        if (t.shape_.size() == 1) {
-            return std::vector<size_t>{1, t.shape_[0]};
-        }
-        return t.shape_;
-    };
-    
-    std::vector<size_t> s1 = to2D(*this);
-    std::vector<size_t> s2 = to2D(other);
-
-    if (!IsBroadcastCompatible(s1, s2)) {
-        throw std::runtime_error("Different shapes for summation"); 
-    }
-
-    std::vector<size_t> final_shape = {std::max(s1[0], s2[0]), std::max(s1[1], s2[1])};
-    Tensor result(final_shape);
-    
-    for (size_t i = 0; i < final_shape[0]; i++) {
-        for (size_t j = 0; j < final_shape[1]; j++) {
-            size_t i1 = (s1[0] == 1) ? 0 : i;
-            size_t j1 = (s1[1] == 1) ? 0 : j;
-            
-            size_t i2 = (s2[0] == 1) ? 0 : i;
-            size_t j2 = (s2[1] == 1) ? 0 : j;
-            
-            result.at({i, j}) = at({i1, j1}) + other.at({i2, j2});
-        }
-    }
+    Tensor result(final_shape, final_data);
     return result;
 }
 
